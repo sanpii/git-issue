5e19f882c9d159f1a8929f35e81592b5c105ff73
Sanpi <sanpi@homecomputing.fr>
2013-08-23 16:28:53 +0200

    git issue edit

---
+++ tests/test_edit.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+setUp()
+{
+    createTestRepository
+    export GIT_EDITOR='echo [test] >'
+}
+
+tearDown()
+{
+    deleteTestRepository
+}
+
+testEdit()
+{
+    local output
+
+    git issue init -q
+    git issue new -q
+
+    output="$(git issue edit 1 2>&1)"
+    local status=$?
+
+    assertEquals 'output' "$(git issue show 1)" '[test]'
+    assertEquals 'status code' $status 0
+}
+
+testEditUnknowId()
+{
+    local output
+
+    git issue init -q
+    git issue new -q
+
+    output="$(git issue edit 2 2>&1)"
+    local status=$?
+
+    assertEquals 'output' "$output" "Issue #2 doesn't exist"
+    assertEquals 'status code' $status 1
+}
+
+CWD="$(cd "$(dirname "$0")" && pwd)"
+
+. $CWD/common.sh
+. $CWD/../shunit2/shunit2
