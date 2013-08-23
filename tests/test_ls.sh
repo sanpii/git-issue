dbdcfea53e55804376127812cb7e3cd519eb916d
Sanpi <sanpi@homecomputing.fr>
2013-08-23 17:12:54 +0200

    git issue list

---
+++ tests/test_ls.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+setUp()
+{
+    createTestRepository
+}
+
+tearDown()
+{
+    deleteTestRepository
+}
+
+testLs()
+{
+    local output
+
+    git issue init -q
+    git issue new -q 'test 1'
+    git issue new -q 'test 2'
+
+    output="$(git issue ls 1 2>&1)"
+    local status=$?
+
+    assertEquals 'output' "$output" '1: test 1
+2: test 2'
+    assertEquals 'status code' $status 0
+}
+
+CWD="$(cd "$(dirname "$0")" && pwd)"
+
+. $CWD/common.sh
+. $CWD/../shunit2/shunit2
