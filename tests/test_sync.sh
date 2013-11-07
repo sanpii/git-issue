dc068f205f3df412b120966f94f2447783282430
Sanpi <sanpi@homecomputing.fr>
2013-11-07 22:20:03 +0100

    Bidirectionnal sync

--- tests/test_sync.sh
+++ tests/test_sync.sh
@@ -13,7 +13,7 @@ tearDown () {
 	deleteTestRepository
 }
 
-testSync () {
+testSyncPush () {
 	local output
 
 	git issue init -q
@@ -30,6 +30,53 @@ testSync () {
 
 	assertEquals 'output' "$output" '1: test 1'
 	assertEquals 'status code' $status 0
+}
+
+testSyncPull () {
+	local output
+
+	cd "$REPO"
+	git issue init -q
+	git issue new -q 'test 1'
+
+	cd "$REPO-clone"
+	output="$(git issue sync 2>&1)"
+	local status=$?
+
+	assertEquals 'output' "$output" 'Issues synced.'
+	assertEquals 'status code' $status 0
+
+	output="$(git issue ls)"
+
+	assertEquals 'output' "$output" '1: test 1'
+	assertEquals 'status code' $status 0
+}
+
+testSyncNothing () {
+	local output
+
+	output="$(git issue sync 2>&1)"
+	local status=$?
+
+	assertEquals 'output' "$output" 'Nothing to sync.'
+	assertEquals 'status code' $status 0
+}
+
+testSyncConflict () {
+	local output
+
+	cd "$REPO"
+	git issue init -q
+	git issue new -q 'test 1' 'Issue in the first repo'
+
+	cd "$REPO-clone"
+	git issue init -q
+	git issue new -q 'test 1' 'Issue in the second repo'
+
+	output="$(git issue sync 2>&1)"
+	local status=$?
+
+	assertEquals 'status code' $status 1
 }
 
 CWD="$(cd "$(dirname "$0")" && pwd)"
