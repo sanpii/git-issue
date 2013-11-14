a63e76669bf150916193c78442cc4dc95bfebd23
Sanpi <sanpi@homecomputing.fr>
2013-11-14 14:43:39 +0100

    Add delete message

--- tests/test_rm.sh
+++ tests/test_rm.sh
@@ -34,6 +34,19 @@ testRmUnknowId () {
 	assertEquals 'status code' $status 1
 }
 
+testRmMessage() {
+	git issue init -q
+	git issue new -q --no-edit
+
+	git issue rm -q -m 'Too old' 1
+	local status=$?
+
+	assertEquals 'output' "$(git show --pretty=format:%B -s issues)" 'Delete issue #1
+
+Too old'
+	assertEquals 'status code' $status 0
+}
+
 CWD="$(cd "$(dirname "$0")" && pwd)"
 
 . $CWD/common.sh
