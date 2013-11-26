b656cd18f5c4cfd19d003dcd0144faa6ab27602c
Sanpi <sanpi@homecomputing.fr>
2013-11-26 17:46:08 +0100

    Add js/main.js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(".issue .description").each(function () {
+    var converter = new Showdown.converter();
+    $(this).html(converter.makeHtml($(this).text()));
+});
+
+$(".issue .expand").on('click', function () {
+    var description = $(this).nextAll(".description");
+    var metadata = $(this).nextAll(".metadata");
+    var expand = $(this).children();
+
+    expand.toggleClass("glyphicon-collapse-up");
+    expand.toggleClass("glyphicon-collapse-down");
+    description.toggle();
+    metadata.toggle();
+});
+$(".issue.status-close .expand").click();
